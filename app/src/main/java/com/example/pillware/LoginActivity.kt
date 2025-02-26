9a2b5704f2973479c485b36e5ae2e6eb2577ef7a
DavidBarragann <dbarragan11@alumnos.uaq.mx>
2025-02-26 13:44:43 -0600

    prueba1

---
+++ app/src/main/java/com/example/pillware/LoginActivity.kt
@@ -0,0 +1,82 @@
+package com.example.pillware
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val editTextUsername = findViewById<EditText>(R.id.usuario)
+        val editTextPassword = findViewById<EditText>(R.id.pass)
+        val buttonLogin = findViewById<Button>(R.id.aceptar)
+        val buttonCancel = findViewById<Button>(R.id.cancel)
+        val textViewTitulo = findViewById<TextView>(R.id.titulo)
+
+        buttonLogin.setOnClickListener {
+            val email = editTextUsername.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signIn(email, password)
+            } else {
+                textViewTitulo.text = "Por favor, ingrese sus credenciales."
+            }
+        }
+        buttonCancel.setOnClickListener {
+            editTextUsername.text.clear()
+            editTextPassword.text.clear()
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("LoginActivity", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    val intento = Intent(this, MainActivity::class.java)
+                    intento.putExtra("EXTRA_TEXTO",email)//donde agregamos al información
+                    startActivity(intento)
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Error de autenticación.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI()
+                }
+            }
+    }
+
+    private fun updateUI() {
+        val textViewTitulo = findViewById<TextView>(R.id.titulo)
+        textViewTitulo.text = "Inicio de sesión fallido"
+    }
+
+    private fun reload() {
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pillware/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.pillware
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import com.example.pillware.ui.theme.PillwareTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val textV=findViewById<TextView>(R.id.mensaje)
+        val texto:String=intent.extras?.getString("EXTRA_TEXTO").orEmpty()
+        textV.text="Bienvenido $texto"
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PillwareTheme {
+        Greeting("Android")
+    }
+}
