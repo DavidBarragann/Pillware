791841e7605706de3cdea3c033dc47c76370d307
DavidBarragann <dbarragan11@alumnos.uaq.mx>
2025-04-28 13:52:29 -0600

    fragmentos, registro, login

--- app/src/main/java/com/example/pillware/ui/locations/LocationsFragment.kt
+++ app/src/main/java/com/example/pillware/ui/locations/LocationsFragment.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.location.Location
 import android.os.Bundle
@@ -12,7 +11,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -27,9 +25,11 @@ import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
@@ -38,8 +38,6 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private val firestore = FirebaseFirestore.getInstance()
-    private val storage = FirebaseStorage.getInstance()
     private var lastKnownLocation: Location? = null
     private val DEFAULT_ZOOM = 15f
     private val LOCATION_PERMISSION_REQUEST_CODE = 123
@@ -81,18 +79,54 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     private fun getDeviceLocationAndNearbyPharmacies() {
-        fusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
-            if (task.isSuccessful && task.result != null) {
-                lastKnownLocation = task.result
-                val currentLatLng = LatLng(task.result.latitude, task.result.longitude)
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM))
-                findNearbyPharmacies(currentLatLng)
-            } else {
-                Log.d(TAG, "Current location is null. Using defaults.")
-                val defaultLocation = LatLng(20.6468, -100.4548)
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM))
-                googleMap.uiSettings.isMyLocationButtonEnabled = false
+        try {
+            val locationResult = fusedLocationClient.lastLocation
+            locationResult.addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    lastKnownLocation = task.result
+                    if (lastKnownLocation != null) {
+                        val currentLatLng = LatLng(
+                            lastKnownLocation!!.latitude,
+                            lastKnownLocation!!.longitude
+                        )
+                        googleMap.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                currentLatLng,
+                                DEFAULT_ZOOM
+                            )
+                        )
+
+                        googleMap.addMarker(
+                            MarkerOptions().position(currentLatLng).title("Tu ubicación")
+                        )
+
+                        findNearbyPharmacies(currentLatLng)
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        val juriquilla = LatLng(20.6468, -100.4548)
+                        googleMap.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                juriquilla,
+                                DEFAULT_ZOOM
+                            )
+                        )
+                        googleMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                } else {
+                    Log.d(TAG, "Failed to get current location.")
+                    Log.e(TAG, "Exception: %s", task.exception)
+                    val juriquilla = LatLng(20.6468, -100.4548)
+                    googleMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            juriquilla,
+                            DEFAULT_ZOOM
+                        )
+                    )
+                    googleMap.uiSettings.isMyLocationButtonEnabled = false
+                }
             }
+        } catch (e: SecurityException) {
+            Log.e("Location", "Location permission not granted: " + e.message)
         }
     }
 
@@ -105,7 +139,10 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
                     "&key=${getString(R.string.google_maps_key)}"
 
         val client = OkHttpClient()
-        val request = Request.Builder().url(url).build()
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -124,23 +161,24 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
                             val place = results.getJSONObject(i)
                             val name = place.getString("name")
 
-                            if (name.contains("veterinaria", ignoreCase = true)) continue
+                            if (name.contains("veterinaria", ignoreCase = true)) {
+                                continue
+                            }
 
                             val geometry = place.getJSONObject("geometry")
                             val locationObj = geometry.getJSONObject("location")
                             val lat = locationObj.getDouble("lat")
                             val lng = locationObj.getDouble("lng")
+
                             val placeLatLng = LatLng(lat, lng)
 
-                            fetchPharmacyLogo(name) { bitmapDescriptor ->
-                                requireActivity().runOnUiThread {
-                                    googleMap.addMarker(
-                                        MarkerOptions()
-                                            .position(placeLatLng)
-                                            .title(name)
-                                            .icon(bitmapDescriptor)
-                                    )
-                                }
+                            requireActivity().runOnUiThread {
+                                googleMap.addMarker(
+                                    MarkerOptions()
+                                        .position(placeLatLng)
+                                        .title(name)
+                                        .icon(getCustomMarkerBitmap())
+                                )
                             }
                         }
                     } catch (e: JSONException) {
@@ -148,43 +186,12 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
                     }
                 }
             }
+
         })
     }
 
-    private fun fetchPharmacyLogo(pharmacyName: String, callback: (BitmapDescriptor) -> Unit) {
-        firestore.collection("farmacias")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    val dbName = document.getString("nombre") ?: continue
-                    if (pharmacyName.contains(dbName, ignoreCase = true)) {
-                        val logoUrl = document.getString("logo") ?: break
-                        val storageRef = storage.getReferenceFromUrl(logoUrl)
-                        storageRef.getBytes(1024 * 1024)
-                            .addOnSuccessListener { bytes ->
-                                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                                val markerBitmap = getCustomMarkerBitmap(bitmap)
-                                callback(markerBitmap)
-                            }
-                            .addOnFailureListener { exception ->
-                                Log.e(TAG, "Error loading logo from Firebase Storage: ${exception.message}")
-                                callback(getDefaultMarkerBitmap())
-                            }
-                        return@addOnSuccessListener
-                    }
-                }
-                callback(getDefaultMarkerBitmap())
-            }
-            .addOnFailureListener { exception ->
-                Log.e(TAG, "Error fetching pharmacy logo: ${exception.message}")
-                callback(getDefaultMarkerBitmap())
-            }
-    }
-
-    private fun getCustomMarkerBitmap(logoBitmap: Bitmap): BitmapDescriptor {
+    private fun getCustomMarkerBitmap(): BitmapDescriptor {
         val customMarkerView = LayoutInflater.from(requireContext()).inflate(R.layout.pharmacy_marker, null)
-        val logoImageView = customMarkerView.findViewById<ImageView>(R.id.logo)
-        logoImageView.setImageBitmap(logoBitmap)
 
         customMarkerView.measure(
             View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
@@ -203,28 +210,39 @@ class LocationsFragment : Fragment(), OnMapReadyCallback {
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
-    private fun getDefaultMarkerBitmap(): BitmapDescriptor {
-        return BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (::googleMap.isInitialized) {
-                try {
-                    googleMap.isMyLocationEnabled = true
-                    getDeviceLocationAndNearbyPharmacies()
-                } catch (e: SecurityException) {
-                    Log.e(TAG, "Location permission was granted but still had a SecurityException: ${e.message}")
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (::googleMap.isInitialized) {
+                        try {
+                            googleMap.isMyLocationEnabled = true
+                            getDeviceLocationAndNearbyPharmacies()
+                        } catch (e: SecurityException) {
+                            Log.e(
+                                "Location",
+                                "Location permission was granted but still had a SecurityException: " + e.message
+                            )
+                        }
+                    }
+                } else {
+                    Log.i(TAG, "Location permissions denied.")
+                    val juriquilla = LatLng(20.6468, -100.4548)
+                    googleMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            juriquilla,
+                            DEFAULT_ZOOM
+                        )
+                    )
                 }
             }
-        } else {
-            Log.i(TAG, "Location permissions denied.")
-            val defaultLocation = LatLng(20.6468, -100.4548)
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM))
+
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
 
